5e7013a9bf6fd4fe7a0541d1523be829d2c217dd
Marco Paggetti - marco.paggetti@studio.unibo.it <marco.paggetti@studio.unibo.it>
2023-12-05 16:28:38 +0100

    Completed half of the exercise

--- 111-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 111-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.Set;
 import java.util.stream.Stream;
+import java.util.stream.Stream.Builder;
 
 /**
  *
@@ -31,27 +32,27 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Stream<String> orderedSongNames() {
-        return null;
+        return this.songs.stream().map(song -> song.getSongName()).sorted(); 
     }
 
     @Override
     public Stream<String> albumNames() {
-        return null;
+        return this.albums.keySet().stream();
     }
 
     @Override
     public Stream<String> albumInYear(final int year) {
-        return null;
+        return this.albums.entrySet().stream().filter(album -> album.getValue().equals(year)).map(album ->album.getKey());
     }
 
     @Override
     public int countSongs(final String albumName) {
-        return -1;
+        return (int)this.songs.stream().filter(song -> song.getAlbumName().equals(albumName)).count();
     }
 
     @Override
     public int countSongsInNoAlbum() {
-        return -1;
+        return (int)this.songs.stream().filter(song -> song.getAlbumName().equals(Optional.empty())).count();
     }
 
     @Override
