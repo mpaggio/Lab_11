61ada2ee21e7812179c248a7bdffda03383b5b99
Marco Paggetti <marco.paggetti@studio.unibo.it>
2023-12-08 19:28:48 +0100

    Completed first part

--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -2,7 +2,6 @@ package it.unibo.oop.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,4 +13,79 @@ import javax.swing.SwingUtilities;
  */
 @SuppressWarnings("PMD.AvoidPrintStackTrace")
 public final class ConcurrentGUI extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel();
+    private final JButton stop = new JButton("stop");
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+
+    /**
+     * Builds a new CGUI.
+     */
+    public ConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+        
+        stop.addActionListener((e) -> {
+            agent.stopCounting();
+            stop.setEnabled(false);
+            up.setEnabled(false);
+            down.setEnabled(false);
+        });
+        up.addActionListener((e) -> agent.countUp());
+        down.addActionListener((e) -> agent.countDown());
+    }
+
+    /*
+     * The counter agent is implemented as a nested class. This makes it
+     * invisible outside and encapsulated.
+     */
+    private class Agent implements Runnable {
+        private volatile boolean stop;
+        private volatile boolean up = true;
+        private int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try { 
+                    this.counter += up ? 1 : -1;
+                    final var text = Integer.toString(this.counter);
+                    SwingUtilities.invokeLater(() -> ConcurrentGUI.this.display.setText(text));
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void countUp(){
+            this.up = true;
+        }
+
+        public void countDown(){
+            this.up = false;
+        }
+    }
 }
